81e5e98ac9bae42879eec9fd9ef314ec69952f74
Ryan Cobb <ryan.david.cobb@gmail.com>
2017-02-05 18:55:34 -0700

    more work on view. todo: hookup type ahead, cleanup scss, build payload

--- app/assets/javascripts/matches.js
+++ app/assets/javascripts/matches.js
@@ -9,12 +9,76 @@ var users = new Bloodhound({
   }
 });
 
-// instantiate the typeahead UI
+var Match = {};
+
 $(function() {
   $('.typeahead').typeahead(null, {
     name: 'users',
     displayKey: 'name',
+    valueKey: 'id',
     source: users,
     minLength: 3
   });
+
+  $('.typeahead').on('typeahead:selected', function(event, datum) {
+  console.log(datum);
+  })
+
+  Match = {
+    initialize: function () {
+      this.domMatch = $("#match .games");
+      this.addButton = $("#addGame");
+      this.removeButton = $("#removeGame");
+      this.games = document.getElementsByClassName("game");
+      this.homePlayer = document.getElementById("home-player").getAttribute("data");
+
+      // Add first game
+      this.addGame();
+
+      this.addButton.click(function() { Match.addGame(); });
+      this.removeButton.click(function() { Match.removeGame(); });
+    },
+    gameHTML: function() { 
+      var gameNumber = Match.games.length + 1;
+
+      return `<div class="game game-${gameNumber}">
+                <div class="gameTitle">
+                  Game ${gameNumber}
+                </div>
+                <div class="control-group">
+                  <div class="team home-team">
+                    <div class="control-container">
+                      <label class="control control-radio">
+                        <input type="radio" name="game-${gameNumber}" value="${Match.homePlayer}">
+                        <div class="control-indicator"></div>
+                      </label>
+                    </div>
+                  </div>
+
+                  <div class="team away-team">
+                    <div class="control-container">
+                      <label class="control control-radio">
+                        <input type="radio" name="game-${gameNumber}" value="${Match.homePlayer}">
+                        <div class="control-indicator"></div>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>`},
+    addGame: function() {
+      var gamestoAdd = 0;
+      this.games.length == 0 ? gamestoAdd = 3 : gamestoAdd = 2;
+
+      for(i=0; i < gamestoAdd; i++) {
+        this.domMatch.append(this.gameHTML);
+      }
+    },
+
+    removeGame: function() {
+      if (this.games.length > 1) {
+        $(".game").last().remove();
+        $(".game").last().remove();
+      }
+    }
+  }
 })
